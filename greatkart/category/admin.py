b4f8f0a1dd6e543234ccda9da3284e2d26b3fd11
Ayush <aadarsha0997@gmail.com>
2025-02-27 11:50:58 +0545

    model and accessing through admin page

--- greatkart/category/admin.py
+++ greatkart/category/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Category
+
+
+
+
+admin.site.register(Category)
+
 # Register your models here.
--- greatkart/category/models.py
+++ greatkart/category/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    catogary_name=models.CharField(unique=True, max_length=50)
+    slug=models.CharField(unique=True,max_length=100)
+    description=models.TextField(blank=True,max_length=255)
+    cat_image=models.ImageField(upload_to="photos/categories",blank=True)
+
+    class Meta:
+        verbose_name='category'
+        verbose_name_plural='categories'
+    def __str__(self):
+        return self.catogary_name
+    
+
